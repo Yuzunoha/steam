1fd085a537bea42ebf466ef9ac1bd1a30e34fe88
Yuzunoha <studysteadystandby@gmail.com>
2021-05-29 20:55:22 +0900

    object

--- main.js
+++ main.js
@@ -3,9 +3,14 @@
 'use strict';
 
 const p = console.log;
-const data = require('./data.js')();
 const main = () => {
-  p(data);
+  const data = require('./data.js')();
+  const object = {
+    購入日: 0,
+    品目: 0,
+    金額: 0,
+  };
+  p(object);
 };
 
 main();
