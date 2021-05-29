80d3a7c76dbf2154fa88d03bf0a4b685eb683b1b
Yuzunoha <studysteadystandby@gmail.com>
2021-05-29 21:34:51 +0900

    セーブ

--- main.js
+++ main.js
@@ -24,10 +24,29 @@ const each = (obj, callback /* (value, keys, data) */) => {
   }
   return ret;
 };
+const 購入月整数 = ({ obj }) => Number(obj.purchaseDate.split('-')[1]);
+const n月のオブジェクトを抽出する = ({ objects, n月 }) => objects.filter((obj) => 購入月整数({ obj }) === n月);
+const n月の合計金額 = ({ objects, n月 }) => {
+  return each(objects, (object) => {
+    const 購入月整数 = object.purchaseDate.split('-')[1];
+    if (Number(n月) === Number(購入月整数)) {
+      return object.amountYen;
+    }
+    return 0;
+  }).reduce((a, b) => a + b);
+};
 const main = () => {
   const data = require('./data.js')();
   const objects = getObjectsFromData(data);
-  each(objects, (v) => p(v));
+  const result = {
+    '1月の合計金額': n月の合計金額({ objects, n月: 1 }),
+    '2月の合計金額': n月の合計金額({ objects, n月: 2 }),
+    '3月の合計金額': n月の合計金額({ objects, n月: 3 }),
+    '4月の合計金額': n月の合計金額({ objects, n月: 4 }),
+    '5月の合計金額': n月の合計金額({ objects, n月: 5 }),
+  };
+  // p(result);
+  p(n月のオブジェクトを抽出する({ objects, n月: 5 }));
 };
 
 main();
